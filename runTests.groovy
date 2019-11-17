32c4cbf7f74b56545e00b5802085de68cbcaf460
Rowell Belen <rowell.belen@serverless.com>
2019-11-17 16:09:54 -0700

    Exclude node_modules in search

--- runTests.groovy
+++ runTests.groovy
@@ -1,9 +1,10 @@
-final pattern1 = new FileNameFinder().getFileNames(args ? args[0] : '.', '**/*Test.groovy')
-final pattern2 = new FileNameFinder().getFileNames(args ? args[0] : '.', '**/*test.groovy')
+final pattern1 = new FileNameFinder().getFileNames(args ? args[0] : '.', '**/*Test.groovy', '**/node_modules/**')
+final pattern2 = new FileNameFinder().getFileNames(args ? args[0] : '.', '**/*test.groovy', '**/node_modules/**')
 (pattern1 + pattern2).each { file ->
   println "Running test: $file"
   def proc = "groovy $file".execute()
   println proc.in.text
+  proc.waitFor()
   if (proc.exitValue() != 0) {
     println proc.err.text
   }
